db926e016237c2b96bfd44927ca4c100667bc04f
Rosny Ratsianompo <rosny.rat@onja.org>
2020-10-08 16:30:46 +0100

    worked on the first button

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import style from 'style.css'
+
+function App() {
+    return ( <
+        div >
+        <
+        h1 > Buttons < /h1> <
+        button className = 'default' > Default < /button> < /
+        div >
+    )
+}
+
+export default App
