583d7fecbdf68209193928cf113ed54ae5f4686e
Rosny Ratsianompo <rosny.rat@onja.org>
2020-10-10 11:27:41 +0100

    worked on react-button

--- App.js
+++ App.js
@@ -40,6 +40,59 @@ function App() {
                     paragraph="<Button disabled />"
                 >Disabled
                 </Button>
+                <Button
+                    variant="text" disabled
+                    paragraph='<Button variant="text" disabled />'
+                >Disabled
+                </Button>
+            </div>
+            <div className="wrapper">
+                <Button
+                    startIcon = "local_grocery_store"
+                    paragraph='<Button startIcon="local_grocery_store" />'
+                >Default
+                </Button>
+                <Button
+                    endIcon = "local_grocery_store"
+                    paragraph='<Button startIcon="local_grocery_store" />'
+                >Default
+                </Button>
+            </div>
+            <div className="wrapper">
+                <Button 
+                size="sm"
+                paragraph='<Button size="sm"/>'
+                >Default</Button>
+                <Button 
+                size="md"
+                paragraph='<Button size="md"/>'
+                >Default</Button>
+                <Button
+                size="lg"
+                paragraph='<Button size="lg"/>'
+                >Default</Button>
+            </div>
+            <div className="wrapper">
+                <Button 
+                size="md"
+                color="default"
+                paragraph='<Button color="default"/>'
+                >Default</Button>
+                <Button 
+                size="md"
+                color="primary"
+                paragraph='<Button color="primary"/>'
+                >Default</Button>
+                <Button
+                size="xlg"
+                color="secondary"
+                paragraph='<Button color="secondary"/>'
+                >Secondary</Button>
+                <Button
+                size="md"
+                color="danger"
+                paragraph='<Button color="danger"/>'
+                >Danger</Button>
             </div>
         </main>
     )
--- Button.js
+++ Button.js
@@ -5,7 +5,22 @@ function Button(props) {
     const variantOutline = props.variant === "outline" ? "outline" : " ";
     const variantText = props.variant === "text" ? "variant-text" : " ";
     const disableShadow = props.disableShadow ? "disable-shadow" : " ";
-    const disabledButton = props.disabled === "disabled" ? " disabled-btn" : " ";
+    const disabledButton = props.disabled ? "disabled-button" : "";
+    const variantTextDisabled = props.variant=== "text" ? "text-disabled" : "";
+    const svgImage = <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+    const startIcon = props.startIcon === "local_grocery_store" ? svgImage : "";
+    const endIcon = props.endIcon === "local_grocery_store" ? svgImage : "";
+    const buttonWithIcon = props.startIcon || props.endIcon === "local_grocery_store" ? "button-with-icon" : "";
+    const smallBtn = props.size === "sm" ?  "small-button" : "";
+    const mediumBtn = props.size === "md" ? "medium-button" : "";
+    const largeBtn = props.size === "lg" ? "large-button" : "";
+    const defaultColor = props.color === "default" ? "default" : "";
+    const primaryBtn = props.color === "primary" ? "primary" : "";
+    const secondaryBtn = props.color === "secondary" ? "secondary" : "";
+    const dangerBtn = props.color === "danger" ? "danger" : "";
+
+
+
     return (
         <div className="button-item">
             <p>{props.paragraph}</p>
@@ -16,9 +31,18 @@ function Button(props) {
                 ${variantText}
                 ${disableShadow}
                 ${disabledButton}
+                ${variantTextDisabled}
+                ${buttonWithIcon}
+                ${smallBtn}
+                ${mediumBtn}
+                ${largeBtn}
+                ${defaultColor}
+                ${primaryBtn}
+                ${secondaryBtn}
+                ${dangerBtn}
             `}
 
-            >{props.children}</button>
+    >{startIcon} {props.children} {endIcon}</button>
         </div>
     )
 }
