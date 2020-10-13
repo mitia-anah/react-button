0a07f0eb84d9819017c5aa24d7a3d9d74d2fa2a8
Rosny Ratsianompo <rosny.rat@onja.org>
2020-10-13 09:44:59 +0100

    updated the title of the react button

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54614" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54672" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
